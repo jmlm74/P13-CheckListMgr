238da57e9e16d3b844996ddc722fd05a7018d146
jmlm74 <jmlm74@gmail.com>
2020-09-28 15:09:24 +0200

    CI - Add SECRET_KEY for Travis !

--- checklistmgr/checklistmgr/travis_settings.py
+++ checklistmgr/checklistmgr/travis_settings.py
@@ -1,7 +1,8 @@
 import os
 
 from .settings import *
-
+# just for Travis !!!
+SECRET_KEY = get_env_variable('SECRET_KEY', "nxx(or0igwys!l7q60i2c_3a36v59k2qtx3ze%4wn#-!18hn#b")
 # postgres database
 DATABASES = {
     'default': {
